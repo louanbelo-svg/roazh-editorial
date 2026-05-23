38ffeac6c5fb8514a079e8fb730d2dee747374b7
louanbelo-svg <louanbelo@gmail.com>
2026-05-23 20:15:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+const fades = document.querySelectorAll('.fade');
 
+const observer = new IntersectionObserver(entries => {
+
+  entries.forEach(entry => {
+
+    if(entry.isIntersecting){
+      entry.target.classList.add('visible');
+    }
+
+  });
+
+},{
+  threshold:0.15
+});
+
+fades.forEach(fade => {
+  observer.observe(fade);
+});
+
+const glow = document.querySelector('.cursor-glow');
+
+document.addEventListener('mousemove', e => {
+
+  glow.style.left = e.clientX + 'px';
+  glow.style.top = e.clientY + 'px';
+
+});
